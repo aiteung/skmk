153691a0762c95df08a3b662b51f909fbc54f779
Valen Rionald <vrionald@gmail.com>
2023-12-12 10:26:28 +0700

    Feat(Time): Tambah function convert bulan ke romawi

---
+++ time.go
@@ -0,0 +1,24 @@
+package skmk
+
+import "time"
+
+func convertRomanMonth() string {
+	currentMonth := time.Now().Month()
+	// Konversi bulan ke format Romawi
+	romanMonths := map[time.Month]string{
+		time.January:   "I",
+		time.February:  "II",
+		time.March:     "III",
+		time.April:     "IV",
+		time.May:       "V",
+		time.June:      "VI",
+		time.July:      "VII",
+		time.August:    "VIII",
+		time.September: "IX",
+		time.October:   "X",
+		time.November:  "XI",
+		time.December:  "XII",
+	}
+
+	return romanMonths[currentMonth]
+}
