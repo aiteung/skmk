b964d4a0a035fbcd6d913012a000293eb8e995e7
Valen Rionald <vrionald@gmail.com>
2023-12-14 15:41:53 +0700

    Fix(Keyword): Perbaiki indeks array string

--- handler.go
+++ handler.go
@@ -12,49 +12,64 @@ import (
 )
 
 func Handler(urlEmail string, db *sql.DB, Pesan model.IteungMessage) (reply string) {
-	log.Printf("Received message: %s", Pesan.Message)
-	// Cek apakah pesan mengandung kata "minta skmk"
-	if strings.Contains(strings.ToLower(Pesan.Message), "minta skmk") {
-		// Handle logika untuk "minta skmk"
-		dataMhs, err := GetMhsByPhoneNumber(db, Pesan.Phone_number)
-		if err != nil {
-			return MessageLengkapiData()
+	// Cek apakah pesan mengandung kata "minta"
+	if strings.Contains(strings.ToLower(Pesan.Message), "minta") {
+		// Mengekstrak kata setelah "minta" menggunakan ekspresi reguler
+		pesanSplit := strings.Fields(Pesan.Message)
+		if len(pesanSplit) > 1 {
+			// matches[1] berisi kata setelah "minta"
+			if strings.Contains(strings.ToLower(pesanSplit[2]), "skmk") {
+				// Handle logika untuk "minta skmk"
+				dataMhs, err := GetMhsByPhoneNumber(db, Pesan.Phone_number)
+				if err != nil {
+					reply = MessageLengkapiData()
+					return reply
+				}
+
+				tahunakademik, err := GetCurrentAcademicYear(db)
+				if err != nil {
+					reply = "Tidak ada tahun akademik aktif"
+					return reply
+				}
+
+				// Handle logika untuk "minta skmk"
+				SkmkFile := ReplaceSkmk(dataMhs, *tahunakademik, db)
+				baseString := base64.StdEncoding.EncodeToString(SkmkFile)
+				filename := fmt.Sprintf("SKMK - %s - %s.pdf", dataMhs.NamaMhs, dataMhs.Nim)
+
+				attachEmail := Files{
+					Mimetype: "application/pdf",
+					Name:     filename,
+					Base64:   baseString,
+				}
+
+				// log.Printf("Sending email to: %s, Subject: %s", dataMhs.Email, "Surat Keterangan Masih Kuliah ULBI")
+
+				// Send email with attachment
+				StatusSend := SendEmailTo(
+					urlEmail,
+					dataMhs.Email,
+					"Surat Keterangan Masih Kuliah ULBI",
+					fmt.Sprintf("Hai hai haiii Kakak %s, inii iteung bawain SKMK nih buat kakak. Gunakan seperlunya ya kakk.....", dataMhs.NamaMhs),
+					attachEmail)
+
+				if !StatusSend {
+					log.Printf("Error sending email: %t", StatusSend)
+					reply = "Gagal mengirim SKMK melalui email"
+					return reply
+				}
+
+				return MessageBerhasilMintaSkmk(dataMhs)
+			} else {
+				// Handle logika untuk "minta" tanpa "skmk"
+				return "Kirim apa broww???"
+			}
+		} else {
+			// Handle logika jika tidak ada kata setelah "minta"
+			return "Yaiyaa minta, minta nya minta apaa???"
 		}
-
-		tahunakademik, err := GetCurrentAcademicYear(db)
-		if err != nil {
-			return "Tidak ada tahun akademik aktif"
-		}
-
-		// Handle logika untuk "minta skmk"
-		SkmkFile := ReplaceSkmk(dataMhs, *tahunakademik, db)
-		baseString := base64.StdEncoding.EncodeToString(SkmkFile)
-		filename := fmt.Sprintf("SKMK - %s - %s.pdf", dataMhs.NamaMhs, dataMhs.Nim)
-
-		attachEmail := Files{
-			Mimetype: "application/pdf",
-			Name:     filename,
-			Base64:   baseString,
-		}
-
-		// log.Printf("Sending email to: %s, Subject: %s", dataMhs.Email, "Surat Keterangan Masih Kuliah ULBI")
-
-		// Send email with attachment
-		StatusSend := SendEmailTo(
-			urlEmail,
-			dataMhs.Email,
-			"Surat Keterangan Masih Kuliah ULBI",
-			fmt.Sprintf("Hai hai haiii Kakak %s, inii iteung bawain SKMK nih buat kakak. Gunakan seperlunya ya kakk.....", dataMhs.NamaMhs),
-			attachEmail)
-
-		if !StatusSend {
-			log.Printf("Error sending email: %t", StatusSend)
-			return "Gagal mengirim SKMK melalui email"
-		}
-
-		return MessageBerhasilMintaSkmk(dataMhs)
 	} else {
-		// Handle logika jika pesan tidak sesuai dengan ekspektasi
+		// Handle logika jika tidak ada kata "minta" dalam pesan
 		return "Apa yang Kakak minta?"
 	}
 }
